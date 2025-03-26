f5e3465f1cdcf2b5de4c0845ae5c7e420545ba4d
Cristofer Ruan <cristoferruan.21@gmail.com>
2025-03-26 16:58:56 -0300

    *

--- setup/settings.py
+++ setup/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
